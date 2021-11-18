2b2982c9a1c00533caac3171886be302ff1e4796
Noam Prag <noam.prag0@gmail.com>
2021-11-18 11:13:36 +0200

    Added mustache drawing

---
+++ lib/drawings/Mustache.cpp
@@ -0,0 +1,29 @@
+#include <Vector.h>
+#include <Bezier.h>
+
+const Bezier mustacheDrawing[] = {
+    Bezier(Vector(1690, 1800), Vector(-223, -18), Vector(-380, -153), Vector(-620, -535)),
+    Bezier(Vector(-620, -535), Vector(-184, -293), Vector(-347, -364), Vector(-535, -235)),
+    Bezier(Vector(-535, -235), Vector(-53, 36), Vector(-82, 65), Vector(-137, 137)),
+    Bezier(Vector(-137, 137), Vector(-48, 63), Vector(-83, 78), Vector(-130, 55)),
+    Bezier(Vector(-130, 55), Vector(-48, -25), Vector(-55, -56), Vector(-34, -149)),
+    Bezier(Vector(-34, -149), Vector(57, -242), Vector(195, -455), Vector(371, -572)),
+    Bezier(Vector(371, -572), Vector(67, -44), Vector(133, -79), Vector(165, -87)),
+    Bezier(Vector(165, -87), Vector(8, -2), Vector(33, -10), Vector(55, -18)),
+    Bezier(Vector(55, -18), Vector(131, -47), Vector(372, -51), Vector(575, -11)),
+    Bezier(Vector(575, -11), Vector(260, 53), Vector(421, 132), Vector(590, 291)),
+    Bezier(Vector(590, 291), Vector(590, 291), Vector(55, 52), Vector(55, 52)),
+    Bezier(Vector(55, 52), Vector(55, 52), Vector(72, -68), Vector(72, -68)),
+    Bezier(Vector(72, -68), Vector(137, -129), Vector(269, -202), Vector(443, -246)),
+    Bezier(Vector(443, -246), Vector(148, -37), Vector(166, -40), Vector(310, -53)),
+    Bezier(Vector(310, -53), Vector(421, -37), Vector(745, 141), Vector(914, 503)),
+    Bezier(Vector(914, 503), Vector(47, 100), Vector(90, 258), Vector(82, 299)),
+    Bezier(Vector(82, 299), Vector(-9, 47), Vector(-33, 67), Vector(-78, 67)),
+    Bezier(Vector(-78, 67), Vector(-30, 0), Vector(-45, -6), Vector(-64, -27)),
+    Bezier(Vector(-64, -27), Vector(-160, -182), Vector(-238, -236), Vector(-340, -234)),
+    Bezier(Vector(-340, -234), Vector(-119, 1), Vector(-240, 92), Vector(-338, 255)),
+    Bezier(Vector(-338, 255), Vector(-193, 318), Vector(-361, 498), Vector(-518, 553)),
+    Bezier(Vector(-518, 553), Vector(-103, 36), Vector(-326, 33), Vector(-428, -6)),
+    Bezier(Vector(-428, -6), Vector(-54, -21), Vector(-64, -21), Vector(-95, -4)),
+    Bezier(Vector(-95, -4), Vector(-50, 27), Vector(-194, 42), Vector(-315, 33)),
+};
