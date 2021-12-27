e1691ddcd9f48bcf5c5b2dd3dd6f40181e63c390
Noam Prag <noam.prag0@gmail.com>
2021-12-27 12:40:42 +0200

    Tuned center and radius

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,16 @@
 #include <Vector.h>
 #include <ServoUtil.h>
 
-const Bezier paths[] = {};
 const float dt = 0.05;
-
-const Vector positions[] = {Vector(10, 20), Vector(10, 30), Vector(-10, 30), Vector(-10, 20), Vector(10, 20)};
 
 void setup()
 {
     // Reset
     attachServos();
-
-    // Trace Bezier paths by evaluating in small steps and sending angles to servos
-    // for (const Bezier path : paths)
-    // {
-    //     for (float t = 0; t <= 1; t += dt)
-    //     {
-    //         const Vector currentWantedPosition = path.evaluate(t);
-    //         const float rightServoWantedAngle = getServoAngle(currentWantedPosition, rightServoPosition);
-    //         const float leftServoWantedAngle = getServoAngle(currentWantedPosition, leftServoPosition);
-
-    //         rightServo.write(rightServoWantedAngle);
-    //         leftServo.write(leftServoWantedAngle);
-    //     }
-    // }
 };
 
-const Vector circleCenter = Vector(15, 15);
-const float circleRadius = 8;
+const Vector circleCenter = Vector(0, 20);
+const float circleRadius = 5;
 
 void loop()
 {
